bab276a6beb1ed589ec51529292ef62dde50b4db
Daffa Damar <daffaardhika@gmail.com>
2022-06-28 16:49:51 +0700

    edit banyak

--- script.js
+++ script.js
@@ -4,13 +4,3 @@ ham.addEventListener('click', () => {
     ham.classList.toggle("change");
     nav.classList.toggle("slide")
 });
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
